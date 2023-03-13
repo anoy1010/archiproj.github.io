0601aa329a27ff1929c24a41cd5bb42c77a689b8
Job Noh <jojoletag@gmail.com>
2023-03-13 13:12:41 +0100

    js

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const sideBar = document.querySelector('.sideBar')
+function onToggleMenu(e) {
+    e.name = e.name === 'menu' ? 'close' : 'menu'
+    e.name = e.name === 'close' ? 'menu' : 'close'
+
+    sideBar.classList.toggle('hidden')
+}
+
